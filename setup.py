ee7683973d0691eafd330a3746177bdb8aea9c9f
NamanVerma27 <namanv417@gmail.com>
2025-09-30 21:32:11 +0530

    Initial commit of project code and requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages , setup
+
+setup(
+    name="sensor",
+    version="0.1.0",
+    author="Naman",
+    author_email="namanv417@gmail.com",
+    packages = find_packages(),
+)
