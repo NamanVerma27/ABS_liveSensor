588039b0f9d40c7457e5391ce74e804b00b88519
NamanVerma27 <namanv417@gmail.com>
2025-10-01 19:29:35 +0530

    changes

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import find_packages , setup
 
+def get_requirements() -> list(str):
+    requirements_list = list[str] = []
+    return requirements_list
+
 setup(
     name="sensor",
     version="0.1.0",
     author="Naman",
     author_email="namanv417@gmail.com",
     packages = find_packages(),
+
+    install_requires = get_requirements()
 )
