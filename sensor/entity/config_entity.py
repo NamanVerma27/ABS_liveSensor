554674712ca58ec4069bb25746373de1679b51a1
NamanVerma27 <namanv417@gmail.com>
2025-10-12 00:49:43 +0530

    created centralized file

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import os , sys
-from sensor.constant.training_pipeline import PIPELINE_NAME , ARTIFACT_DIR , DATA_INGESTION_DIR_NAME , FILE_NAME , DATA_INGESTION_FEATURE_STORE_DIR , DATA_INGESTION_INGESTED_DIR , TEST_FILE_NAME
+from sensor.constant.training_pipeline import PIPELINE_NAME , ARTIFACT_DIR , DATA_INGESTION_DIR_NAME , FILE_NAME , DATA_INGESTION_FEATURE_STORE_DIR , DATA_INGESTION_INGESTED_DIR , TEST_FILE_NAME , DATA_INGESTION_COLLECTION_NAME , TRAIN_FILE_NAME , DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
 from sensor.exception import SensorException
 
 class TrainingPipelineConfig:
@@ -19,10 +19,10 @@ class DataIngestionConfig:
         def __init__(self , training_pipeline_config : TrainingPipelineConfig):
             self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , DATA_INGESTION_DIR_NAME)
             self.feature_store_file_path = os.path.join(self.data_ingestion_dir , DATA_INGESTION_FEATURE_STORE_DIR , FILE_NAME)
-            self.train_file_path = os.path.join(self.data_ingestion_dir , DATA_INGESTION_INGESTED_DIR , "train" , TEST_FILE_NAME)
+            self.train_file_path = os.path.join(self.data_ingestion_dir , DATA_INGESTION_INGESTED_DIR , "train" , TRAIN_FILE_NAME)
             self.test_file_path = os.path.join(self.data_ingestion_dir , DATA_INGESTION_INGESTED_DIR , "test" , TEST_FILE_NAME)
-            self.test_size = 0.2
-            self.collection_name = "sensor"
+            self.test_size = DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
+            self.collection_name: str = DATA_INGESTION_COLLECTION_NAME
 
     except Exception as e:
         raise SensorException(e , sys)
