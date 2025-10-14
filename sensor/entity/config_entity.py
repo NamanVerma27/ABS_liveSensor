d4b49cb932aed29d339319af2c1c241e36c0467c
NamanVerma27 <namanv417@gmail.com>
2025-10-14 22:54:26 +0530

    data validation configuration setted up

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import os , sys
 from sensor.constant.training_pipeline import PIPELINE_NAME , ARTIFACT_DIR , DATA_INGESTION_DIR_NAME , FILE_NAME , DATA_INGESTION_FEATURE_STORE_DIR , DATA_INGESTION_INGESTED_DIR , TEST_FILE_NAME , DATA_INGESTION_COLLECTION_NAME , TRAIN_FILE_NAME , DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
+from sensor.constant.training_pipeline import DATA_VALIDATION_DIR_NAME , DATA_VALIDATION_VALID_DIR , DATA_VALIDATION_INVALID_DIR , DATA_VALIDATION_DRIFT_REPORT_DIR , DATA_VALIDATION_DRIFT_REPORT_FILE_NAME
 from sensor.exception import SensorException
 
 class TrainingPipelineConfig:
@@ -24,5 +25,23 @@ class DataIngestionConfig:
             self.test_size = DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
             self.collection_name: str = DATA_INGESTION_COLLECTION_NAME
 
+    except Exception as e:
+        raise SensorException(e , sys)
+    
+class DataValidationConfig:
+
+    try:
+        def __init__(self , training_pipeline_config : TrainingPipelineConfig):
+            self.data_validation_dir = os.path.join(training_pipeline_config.artifact_dir , DATA_VALIDATION_DIR_NAME)
+            self.valid_data_dir = os.path.join(self.data_validation_dir , DATA_VALIDATION_VALID_DIR)
+            self.invalid_data_dir = os.path.join(self.data_validation_dir , DATA_VALIDATION_INVALID_DIR)
+            self.valid_train_file_path = os.path.join(self.valid_data_dir , TRAIN_FILE_NAME)
+            self.valid_test_file_path = os.path.join(self.valid_data_dir , TEST_FILE_NAME)
+            self.invalid_train_file_path = os.path.join(self.invalid_data_dir , TRAIN_FILE_NAME)
+            self.invalid_test_file_path = os.path.join(self.invalid_data_dir , TEST_FILE_NAME)
+            
+            self.drift_report_dir = os.path.join(self.data_validation_dir , DATA_VALIDATION_DRIFT_REPORT_DIR)
+            self.drift_report_file_path = os.path.join(self.drift_report_dir , DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
+        
     except Exception as e:
         raise SensorException(e , sys)
