f085b3e0b545ef11cd68d18c1687bfd30632cef9
NamanVerma27 <namanv417@gmail.com>
2025-11-15 17:35:45 +0530

    utils file completed

--- sensor/utils/main_utils.py
+++ sensor/utils/main_utils.py
@@ -1,5 +1,10 @@
 import os , sys
+import dill
+
+import numpy as np
 import yaml
+
+from sensor.logger import logging
 from sensor.exception import SensorException
 
 def read_yaml_file(file_path: str) -> dict:
@@ -26,5 +31,69 @@ def write_yaml_file(file_path: str , content: object , replace: bool = False) ->
         with open(file_path , 'w') as yaml_file:
             yaml.dump(content , yaml_file)
     
+    except Exception as e:
+        raise SensorException(e , sys)
+    
+def save_numpy_array_data(file_path: str , array: np.ndarray) -> None:
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path , exist_ok=True)
+        with open(file_path , 'wb') as file_obj:
+            np.save(file_obj , array)
+        
+    except Exception as e:
+        raise SensorException(e, sys)
+    
+def load_numpy_array_data(file_path: str) -> np.ndarray:
+    """
+    Load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+
+    try:
+        with open(file_path , 'rb') as file_obj:
+            return np.load(file_obj)
+        
+    except Exception as e:
+        raise SensorException(e , sys)
+    
+def save_object(file_path: str , obj: object) -> None:
+    """
+    save a python object to file
+    file_path: str location of file to save
+    obj: object to be saved
+    """
+
+    try:
+        logging.info("Entered the save_object method of MainUtils class")
+        os.makedirs(os.path.dirname(file_path) , exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj , file_obj)
+        logging.info("Exited the save_object method of MainUtils class")
+
+    except Exception as e:
+        raise SensorException(e , sys)
+    
+def load_object(file_path: str) -> object:
+    """
+    load a python object from file
+    file_path: str location of file to load
+    return: object loaded
+    """
+
+    try:
+        logging.info("Entered the load_object method of MainUtils class")
+        with open(file_path , 'rb') as file_obj:
+            obj = dill.load(file_obj)
+        logging.info("Exited the load_object method of MainUtils class")
+        return obj
+
     except Exception as e:
         raise SensorException(e , sys)
